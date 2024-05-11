6e633dea41e45e01e1ffc93318d468d59eb67972
kyleqihua <kyleqihuahuang@gmail.com>
2024-05-11 13:29:46 +0800

    bare layout, navbar, footer done

---
+++ src/app/not-found.jsx
@@ -0,0 +1,14 @@
+import Link from "next/link";
+
+const NotFound = () => {
+  return (
+    <div>
+      <div>
+        <h2>NotFound</h2>
+      </div>
+      <Link href="/">Return Homepage</Link>
+    </div>
+  );
+};
+
+export default NotFound;
