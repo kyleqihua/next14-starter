c0e1f87a84052424332e531001a29cd9aadf4ba8
kyleqihua <kyleqihuahuang@gmail.com>
2024-05-11 21:21:36 +0800

    active class for navbar link

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,20 +1,30 @@
-import Link from "next/link";
 import styles from "./links.module.css";
+import NavLink from "./navLink/NavLink";
 
 const Links = () => {
   const links = [
-    { title: "Homepage", path: "/>" },
+    { title: "Homepage", path: "/" },
     { title: "About", path: "/about" },
     { title: "Contact", path: "/contact" },
     { title: "Blog", path: "/blog" },
   ];
+
+  //temporary data
+  const session = true;
+  const isAdmin = true;
   return (
     <div className={styles.links}>
       {links.map((link) => (
-        <Link href={link.path} key={link.title}>
-          {link.title}
-        </Link>
-      ))}
+        <NavLink item={link} key={link.title} />
+      ))}{" "}
+      {session ? (
+        <>
+          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+          <button className={styles.logout}>Logout</button>
+        </>
+      ) : (
+        <NavLink item={{ title: "Login", path: "/login" }} />
+      )}
     </div>
   );
 };
