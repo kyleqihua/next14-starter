2736a282540f5da7d3a462d5a8f58a83ce866e3e
kyleqihua <kyleqihuahuang@gmail.com>
2024-05-12 23:07:13 +0800

    responsive container layout done

--- src/components/navbar/links/Links.jsx
+++ src/components/navbar/links/Links.jsx
@@ -1,29 +1,53 @@
+"use client";
+import { useState } from "react";
 import styles from "./links.module.css";
 import NavLink from "./navLink/NavLink";
 
+const links = [
+  { title: "Homepage", path: "/" },
+  { title: "About", path: "/about" },
+  { title: "Contact", path: "/contact" },
+  { title: "Blog", path: "/blog" },
+];
+
 const Links = () => {
-  const links = [
-    { title: "Homepage", path: "/" },
-    { title: "About", path: "/about" },
-    { title: "Contact", path: "/contact" },
-    { title: "Blog", path: "/blog" },
-  ];
+  const [open, setOpen] = useState(false);
 
   //temporary data
   const session = true;
   const isAdmin = true;
+
   return (
-    <div className={styles.links}>
-      {links.map((link) => (
-        <NavLink item={link} key={link.title} />
-      ))}{" "}
-      {session ? (
-        <>
-          {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
-          <button className={styles.logout}>Logout</button>
-        </>
-      ) : (
-        <NavLink item={{ title: "Login", path: "/login" }} />
+    <div className={styles.container}>
+      <div className={styles.links}>
+        {links.map((link) => (
+          <NavLink item={link} key={link.title} />
+        ))}{" "}
+        {/* write a condition for authentication */}
+        {session ? (
+          <>
+            {/* write another condition for admin */}
+            {isAdmin && <NavLink item={{ title: "Admin", path: "/admin" }} />}
+            <button className={styles.logout}>Logout</button>
+          </>
+        ) : (
+          <NavLink item={{ title: "Login", path: "/login" }} />
+        )}
+      </div>
+      {/* create a button for the mobile menu. add the set state of open */}
+      <button
+        className={styles.menuButton}
+        onClick={() => setOpen((prev) => !prev)}
+      >
+        Menu
+      </button>
+      {/* check if the menu button is open. if open, map again */}
+      {open && (
+        <div className={styles.mobileLinks}>
+          {links.map((link) => (
+            <NavLink item={link} key={link.title} />
+          ))}
+        </div>
       )}
     </div>
   );
