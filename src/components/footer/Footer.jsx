79106f4d3d890951339731d7b887cd8a38e38d38
kyleqihua <kyleqihuahuang@gmail.com>
2024-05-13 01:28:42 +0800

    homepage design done

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,5 +1,12 @@
-const Footer = () => {
-  return <div>Footer</div>;
-};
+import styles from "./footer.module.css"
 
-export default Footer;
+const Footer = () => {
+  return (
+    <div className={styles.container}>
+      <div className={styles.logo}>Lorem</div>
+      <div className={styles.text}>Lama agency © All rights reserved.</div>
+    </div>
+  )
+}
+
+export default Footer
